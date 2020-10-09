84f521bea1dc820fa90d68249624906b38a0d614
Arvinder8002 <arvindersingh8002@gmail.com>
2020-10-09 17:31:03 +0530

    added custom mouse and click effects

--- websiteScript.js
+++ websiteScript.js
@@ -119,20 +119,55 @@ theme.addEventListener('click',()=>{
   theme.classList.toggle('show-dropdown');
 })
 
-//decreasing text size on scroll variables
-const headingH3 = document.querySelectorAll('.heading h3');
-const maxSize = 65;
-const minSize = 40;
-const paraMin = 22;
-const paraMax = 32.5;
-let workHeadingText = maxSize;
-let aboutHeadingText = maxSize;
-let servicesHeadingText = maxSize;
-let contactHeadingText = maxSize;
-let testimonialHeadingText = maxSize;
-let servicesParaText = paraMax;
-let contactParaText = paraMin;
-let prevScrollY;
+//setting the cursor
+const cursor = document.querySelector('#cursor');
+window.addEventListener('mousemove', event =>{
+  cursor.style.top = event.clientY +'px';
+  cursor.style.left = event.clientX +'px';
+});
+
+//expanding cursor on hover of a
+const links = document.querySelectorAll('nav a');
+links.forEach(link=>{
+  link.addEventListener('mouseenter',()=>{
+    link.style.color = 'var(--gradientColor1)';
+    cursor.classList.add('mouse-enter');
+    cursor.style.width = (link.getBoundingClientRect().width - 28)+'px';
+    cursor.style.borderRadius = '30px';
+  });
+  link.addEventListener('mouseleave',()=>{
+    link.style.color = 'var(--textColor)';
+    cursor.classList.remove('mouse-enter'); 
+    cursor.style.borderRadius = '50%';
+    cursor.style.width = '20px'; 
+  });
+
+});
+ 
+const allOtherA = document.querySelectorAll('a');
+for(let i = 5; i < allOtherA.length; i++){
+ allOtherA[i].addEventListener('mouseenter',()=>{
+   cursor.style.opacity = 0;
+ });
+ allOtherA[i].addEventListener('mouseleave',()=>{
+   cursor.style.opacity = 1;
+ })
+}
+
+lines.addEventListener('mouseenter',()=>{
+  line.classList.add('line-mouse-enter');
+    cursor.style.width = '50px';
+    cursor.style.height = '50px';
+    cursor.classList.add('mouse-enter');
+  
+});
+lines.addEventListener('mouseleave',()=>{
+  line.classList.remove('line-mouse-enter');
+    cursor.classList.remove('mouse-enter');
+    cursor.style.width = '20px';
+    cursor.style.height = '20px';
+  
+})
 
 //scroll animations on all elements
 function appear(){
@@ -200,79 +235,154 @@ function appear(){
   {
     others.classList.add('slide-left');
   }
-
-  //decreasing text size on scroll
-  const workHeading = document.getElementById('work-heading');
-  if(workHeading.getBoundingClientRect().top < screenHeight/2){
-    workHeading.style.fontSize = workHeadingText+'px';
-    if(workHeadingText > minSize)  workHeadingText -= (window.scrollY - prevScrollY)/9;
+  
+  const headingH3s = document.querySelectorAll('.heading h3');
+  headingH3s.forEach(heading=>{
+    heading.style.opacity = 0;
+  if(heading.getBoundingClientRect().top < screenHeight + 50)
+  {
+    heading.classList.add('scroll-appear');
   }
-  else{
-    if(workHeadingText < maxSize)  workHeadingText -=  (window.scrollY - prevScrollY)/9;
-    workHeading.style.fontSize = workHeadingText+'px';
+  })
+  const headingPs = document.querySelectorAll('.heading p');
+  headingPs.forEach(para=>{
+    para.style.opacity = 0;
+  if(para.getBoundingClientRect().top < screenHeight + 50)
+  {
+    para.classList.add('scroll-appear');
   }
-
-  const aboutHeading = document.getElementById('about-heading');
-  if(aboutHeading.getBoundingClientRect().top < screenHeight/2){
-   aboutHeading.style.fontSize = aboutHeadingText+'px';
-    if(aboutHeadingText > minSize) aboutHeadingText -=  (window.scrollY - prevScrollY)/9;
-  }
-  else{
-    if(aboutHeadingText < maxSize)  aboutHeadingText -=  (window.scrollY - prevScrollY)/9;
-    aboutHeading.style.fontSize = aboutHeadingText+'px';
-  }
-
-  const servicesHeading = document.getElementById('services-heading');
-  if(servicesHeading.getBoundingClientRect().top < screenHeight/2){
-    servicesHeading.style.fontSize = servicesHeadingText+'px';
-    if(servicesHeadingText > minSize) servicesHeadingText -=  (window.scrollY - prevScrollY)/9;
-  }
-  else{
-    if(servicesHeadingText < maxSize)  servicesHeadingText -=  (window.scrollY - prevScrollY)/9;
-    servicesHeading.style.fontSize = servicesHeadingText+'px';
-  }
-    
-  const testimonialHeading = document.getElementById('testimonial-heading');
-  if(testimonialHeading.getBoundingClientRect().top < screenHeight/2){
-    testimonialHeading.style.fontSize = testimonialHeadingText+'px';
-    if(testimonialHeadingText > minSize) testimonialHeadingText -=  (window.scrollY - prevScrollY)/9;
-  }
-  else{
-    if(testimonialHeadingText < maxSize)  testimonialHeadingText -=  (window.scrollY - prevScrollY)/9;
-    testimonialHeading.style.fontSize = testimonialHeadingText+'px';
-  }
-
-  const contactHeading = document.getElementById('contact-heading');
-  if(contactHeading.getBoundingClientRect().top < screenHeight/2){
-    contactHeading.style.fontSize = contactHeadingText+'px';
-    if(contactHeadingText > minSize) contactHeadingText -=  (window.scrollY - prevScrollY)/9;
-  }
-  else{
-    if(contactHeadingText < maxSize)  contactHeadingText -=  (window.scrollY - prevScrollY)/9;
-    contactHeading.style.fontSize = contactHeadingText+'px';
-  }
-
-  const servicesPara  = document.querySelector('#services-para');
-  if(servicesPara.getBoundingClientRect().top < screenHeight/1.5){
-    servicesPara.style.fontSize = servicesParaText+'px';
-    if(servicesParaText > paraMin) servicesParaText -=  (window.scrollY - prevScrollY)/9;
-  }
-  else{
-    if(servicesParaText < paraMax) servicesParaText -=  (window.scrollY - prevScrollY)/9;
-    servicesPara.style.fontSize = servicesParaText+'px';
-  }
-
-  const contactPara  = document.querySelector('#contact-para');
-  if(contactPara.getBoundingClientRect().top < screenHeight/1.5){
-    contactPara.style.fontSize = contactParaText+'px';
-    if(contactParaText > paraMin) contactParaText -= (window.scrollY - prevScrollY)/9;
-  }
-  else{
-    if(contactParaText < paraMax) contactParaText += 0.6;
-    contactPara.style.fontSize = contactParaText+'px';
-  }
-
-  prevScrollY = window.scrollY;
+  });
+  
+  // prevScrollY = window.scrollY;
 }
 window.addEventListener('scroll',appear);
+ 
+//bubbles effect on click
+window.addEventListener('click',spawn);
+let particles = [];
+function spawn(e){
+for(let i = 0; i < 50; i++) {
+  const bubble = document.createElement('particle');
+  particles.push(bubble);
+  document.body.appendChild(bubble);
+  bubble.style.top = e.pageY +'px';
+  bubble.style.left = e.pageX +'px';
+  const size = Math.floor(Math.random()*15 + 5);
+  bubble.style.height = size+'px';
+  bubble.style.width = size +'px';
+  bubble.style.background = `hsl(${Math.random()*360},50%,50%)`;
+  const lastX = `${(Math.random() - 0.5)*2*100}px`;
+  const lastY = `${(Math.random() - 0.5)*2*100}px`;
+  bubble.style.setProperty('--lastX',lastX);
+  bubble.style.setProperty('--lastY',lastY);
+}
 
+//removing particles
+particles.forEach((particle,i)=>{
+  if(true){
+    particles.splice(i,1);
+    document.body.removeChild(particle);  
+  } 
+  })
+}
+
+window.addEventListener('resize',()=>{
+  if(innerWidth > 990){
+    const navLinks = ul.querySelectorAll('a');
+    navLinks.forEach( link=>{
+      link.style.padding = `5px 18px`;
+    })
+  }
+})
+
+
+
+//REMOVED FEATURES
+  
+  //decreasing text size on scroll variables
+
+  // const maxSize = 65;
+  // const minSize = 40;
+  // const paraMin = 22;
+  // const paraMax = 32.5;
+  // let workHeadingText = maxSize;
+  // let aboutHeadingText = maxSize;
+  // let servicesHeadingText = maxSize;
+  // let contactHeadingText = maxSize;
+  // let testimonialHeadingText = maxSize;
+  // let servicesParaText = paraMax;
+  // let contactParaText = paraMin;
+  // let prevScrollY;
+
+
+  //decreasing text size on scroll
+
+  // const workHeading = document.getElementById('work-heading');
+  // if(workHeading.getBoundingClientRect().top < screenHeight/2){
+  //   workHeading.style.fontSize = workHeadingText+'px';
+  //   if(workHeadingText > minSize)  workHeadingText -= (window.scrollY - prevScrollY)/9;
+  // }
+  // else{
+  //   if(workHeadingText < maxSize)  workHeadingText -=  (window.scrollY - prevScrollY)/9;
+  //   workHeading.style.fontSize = workHeadingText+'px';
+  // }
+
+  // const aboutHeading = document.getElementById('about-heading');
+  // if(aboutHeading.getBoundingClientRect().top < screenHeight/2){
+  //  aboutHeading.style.fontSize = aboutHeadingText+'px';
+  //   if(aboutHeadingText > minSize) aboutHeadingText -=  (window.scrollY - prevScrollY)/9;
+  // }
+  // else{
+  //   if(aboutHeadingText < maxSize)  aboutHeadingText -=  (window.scrollY - prevScrollY)/9;
+  //   aboutHeading.style.fontSize = aboutHeadingText+'px';
+  // }
+
+  // const servicesHeading = document.getElementById('services-heading');
+  // if(servicesHeading.getBoundingClientRect().top < screenHeight/2){
+  //   servicesHeading.style.fontSize = servicesHeadingText+'px';
+  //   if(servicesHeadingText > minSize) servicesHeadingText -=  (window.scrollY - prevScrollY)/9;
+  // }
+  // else{
+  //   if(servicesHeadingText < maxSize)  servicesHeadingText -=  (window.scrollY - prevScrollY)/9;
+  //   servicesHeading.style.fontSize = servicesHeadingText+'px';
+  // }
+    
+  // const testimonialHeading = document.getElementById('testimonial-heading');
+  // if(testimonialHeading.getBoundingClientRect().top < screenHeight/2){
+  //   testimonialHeading.style.fontSize = testimonialHeadingText+'px';
+  //   if(testimonialHeadingText > minSize) testimonialHeadingText -=  (window.scrollY - prevScrollY)/9;
+  // }
+  // else{
+  //   if(testimonialHeadingText < maxSize)  testimonialHeadingText -=  (window.scrollY - prevScrollY)/9;
+  //   testimonialHeading.style.fontSize = testimonialHeadingText+'px';
+  // }
+
+  // const contactHeading = document.getElementById('contact-heading');
+  // if(contactHeading.getBoundingClientRect().top < screenHeight/2){
+  //   contactHeading.style.fontSize = contactHeadingText+'px';
+  //   if(contactHeadingText > minSize) contactHeadingText -=  (window.scrollY - prevScrollY)/9;
+  // }
+  // else{
+  //   if(contactHeadingText < maxSize)  contactHeadingText -=  (window.scrollY - prevScrollY)/9;
+  //   contactHeading.style.fontSize = contactHeadingText+'px';
+  // }
+
+  // const servicesPara  = document.querySelector('#services-para');
+  // if(servicesPara.getBoundingClientRect().top < screenHeight/1.5){
+  //   servicesPara.style.fontSize = servicesParaText+'px';
+  //   if(servicesParaText > paraMin) servicesParaText -=  (window.scrollY - prevScrollY)/9;
+  // }
+  // else{
+  //   if(servicesParaText < paraMax) servicesParaText -=  (window.scrollY - prevScrollY)/9;
+  //   servicesPara.style.fontSize = servicesParaText+'px';
+  // }
+
+  // const contactPara  = document.querySelector('#contact-para');
+  // if(contactPara.getBoundingClientRect().top < screenHeight/1.5){
+  //   contactPara.style.fontSize = contactParaText+'px';
+  //   if(contactParaText > paraMin) contactParaText -= (window.scrollY - prevScrollY)/9;
+  // }
+  // else{
+  //   if(contactParaText < paraMax) contactParaText += 0.6;
+  //   contactPara.style.fontSize = contactParaText+'px';
+  // }
